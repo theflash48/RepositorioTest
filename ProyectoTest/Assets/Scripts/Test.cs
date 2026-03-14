62a92ecd4e604ce58cb82d710c5c818746e8d88b
theflash48 <pabloski.2002@gmail.com>
2026-03-14 20:08:03 +0100

    Change comment from 'Hola' to 'Adios'

--- ProyectoTest/Assets/Scripts/Test.cs
+++ ProyectoTest/Assets/Scripts/Test.cs
@@ -5,7 +5,7 @@ public class Test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Hola
+        // Adios
     }
 
     // Update is called once per frame
