7fd531b607b51115d24d09490b11a73ebc974568
theflash48 <59892110+theflash48@users.noreply.github.com>
2026-03-14 20:08:35 +0100

    Add Test2 script and update Test.cs comment
    Add a new MonoBehaviour script ProyectoTest/Assets/Scripts/Test2.cs (with Start and Update stubs) and its .meta file. Also change a single-line comment in Test.cs from "// Hola" to "// Hola Caracola". Test2 is a placeholder for future logic.

--- ProyectoTest/Assets/Scripts/Test2.cs
+++ ProyectoTest/Assets/Scripts/Test2.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Test2 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Hola Caracola
+        
     }
 
     // Update is called once per frame
